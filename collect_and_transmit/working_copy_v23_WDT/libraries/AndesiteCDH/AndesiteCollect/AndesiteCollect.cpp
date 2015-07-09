ae1f0fb3ab8385184e4d779c815a402b92fae0a0
Zach Vickerson <zachv@bu.edu>
2015-07-09 10:12:47 -0400

    Edits 7/9/15

---
+++ collect_and_transmit/working_copy_v23_WDT/libraries/AndesiteCDH/AndesiteCollect/AndesiteCollect.cpp
@@ -0,0 +1,101 @@
+// -*-c++-*-
+// **********************************************************************************
+// 
+// Name:    AndesiteCollect.cpp
+// Class:   <AndesiteCollect>
+// Author:  Gabriel Gonzalez, Maria Kromis, John McCullough
+// 
+// Description: Provides the functionality to collect and store (not write!) data on
+//              the ANDESITE Satellite.
+//              
+// Notes: Check if converting from double to string correctly.
+// 
+// **********************************************************************************
+
+
+// ========
+// Includes
+// ========
+
+#include "AndesiteCollect.h"
+#include "AndesiteData.h"
+#include "AndesiteFile.h"
+
+
+
+// //////////////////////////////////
+// ///// ANDESITE COLLECT CLASS /////
+// //////////////////////////////////
+
+// Collect and store magnetometer data
+void AndesiteCollect::mag() {
+    DOF.readMag();
+    String data;    
+    char temp[10] = {0};
+    
+    // Concatenate strings to write just one string to file
+    data = "M";
+    //dtostrf(acdh_adc_readData(.....),1,4,temp);
+    dtostrf(DOF.calcMag(DOF.mx), 1, 4, temp);
+    data += temp;
+    data += ",";
+    dtostrf(DOF.calcMag(DOF.my), 1, 4, temp);
+    data += temp;
+    data += ",";
+    dtostrf(DOF.calcMag(DOF.mz), 1, 4, temp);
+    data += temp;
+    
+    _File.store(data);
+}
+
+
+
+// Collect and store gyroscope data
+void AndesiteCollect::gyro() {
+    DOF.readGyro();
+    String data;
+    char temp[10] = {0};
+    
+    // Concatenate strings to write just one string to file
+    data = "G";
+    dtostrf(DOF.calcGyro(DOF.gx), 1, 4, temp);
+    data += temp;
+    data += ",";
+    dtostrf(DOF.calcGyro(DOF.gy), 1, 4, temp);
+    data += temp;
+    data += ",";
+    dtostrf(DOF.calcGyro(DOF.gz), 1, 4, temp);
+    data += temp;
+    
+    _File.store(data);
+}
+
+
+
+// Collect and store GPS data
+void AndesiteCollect::gps() {
+    String data;
+    char temp[10] = {0};
+    
+    // Concatenate strings to write just one string to file
+    data = "T";
+    data += GPS.time.hour();
+    data += ":";
+    data += GPS.time.minute();
+    data += ":";
+    data += GPS.time.second();
+    data += ".";
+    data += GPS.time.centisecond();
+    data += ",";
+    
+    dtostrf(GPS.location.lat(), 1, 4, temp);
+    data += temp;
+    data += ",";
+    dtostrf(GPS.location.lng(), 1, 4, temp);
+    data += temp;
+    data += ",";
+    dtostrf(GPS.altitude.meters(), 1, 4, temp);
+    data += temp;
+    
+    _File.store(data);
+}
---
+++ collect_and_transmit/working_copy_v23_WDT/libraries/WSN/WSN.cpp
@@ -0,0 +1,155 @@
+#include "Arduino.h"
+#include "WSN.h"
+#include <String.h>
+
+
+WSN::WSN()
+{
+	
+}
+
+String WSN::GPSsim(String latitude)
+{
+	Serial.println(latitude);
+	int newlat = (latitude.toInt()) + 10;
+	if (newlat >= 9000)
+	{
+		newlat = 0;
+	}
+	String lat = (String) newlat;
+	return lat;
+}
+
+
+int WSN::OrbitCounter(int &OrbitCounter, String GPS)
+{
+	WSN().sortInfo(GPS);
+
+	if (NS == "N")
+	{
+		if (latitude.toInt() >= 8900)
+		{
+			OrbitCounter++;
+		}
+	}
+	return OrbitCounter;
+}
+
+void WSN::sortInfo(String GPS)//, int value)
+{
+	int firstcomma = GPS.indexOf(',');
+	int secondcomma = GPS.indexOf(',', firstcomma + 1);
+	int thirdcomma = GPS.indexOf(',', secondcomma + 1);
+	int fourthcomma = GPS.indexOf(',', thirdcomma + 1);
+	int fifthcomma = GPS.indexOf(',', fourthcomma + 1);
+	int sixthcomma = GPS.indexOf(',', fifthcomma + 1);
+	int seventhcomma = GPS.indexOf(',', sixthcomma + 1);
+	int eigthcomma = GPS.indexOf(',', seventhcomma + 1);
+	int ninthcomma = GPS.indexOf(',', eigthcomma + 1);
+	int tenthcomma = GPS.indexOf(',', ninthcomma + 1);
+	int eleventhcomma = GPS.indexOf(',', tenthcomma + 1);
+	int twelthcomma = GPS.indexOf(',', eleventhcomma + 1);
+	int thirteenthcomma = GPS.indexOf(',', twelthcomma + 1);
+	int fourteenthcomma = GPS.indexOf(',', thirteenthcomma + 1);
+
+	type = GPS.substring(0, firstcomma);
+	time = GPS.substring(firstcomma + 1, secondcomma);
+	latitude = GPS.substring(secondcomma + 1, thirdcomma);
+	NS = GPS.substring(thirdcomma + 1, fourthcomma);
+	longitude = GPS.substring(fourthcomma + 1, fifthcomma);
+	EW = GPS.substring(fifthcomma + 1, sixthcomma);
+	fixquality = GPS.substring(sixthcomma + 1, seventhcomma);
+	numSat = GPS.substring(seventhcomma + 1, eigthcomma);
+	dilution = GPS.substring(eigthcomma + 1, ninthcomma);
+	altitude = GPS.substring(ninthcomma + 1, tenthcomma);
+	altitudeUnits = GPS.substring(tenthcomma + 1, eleventhcomma);
+	geoidOffset = GPS.substring(eleventhcomma + 1, twelthcomma);
+	geoidOffsetUnits = GPS.substring(twelthcomma + 1, thirteenthcomma);
+	checksum = GPS.substring(thirteenthcomma + 1, fourteenthcomma);
+}
+
+String WSN::getType()
+{
+	return type;
+}
+
+String WSN::getTime()
+{
+	return time;
+}
+
+String WSN::getLatitude()
+{
+	return latitude;
+}
+
+String WSN::getNS()
+{
+	return NS;
+}
+
+String WSN::getLongitude()
+{
+	return longitude;
+}
+
+String WSN::getEW()
+{
+	return EW;
+}
+
+String WSN::getFixQuality()
+{
+	return fixquality;
+}
+
+String WSN::getNumSat()
+{
+	return numSat;
+}
+
+String WSN::getDilution()
+{
+	return dilution;
+}
+
+String WSN::getAltitude()
+{
+	return altitude;
+}
+
+String WSN::getAltitudeUnits()
+{
+	return altitudeUnits;
+}
+
+String WSN::getOffset()
+{
+	return geoidOffset;
+}
+
+String WSN::getOffsetUnits()
+{
+	return geoidOffsetUnits;
+}
+
+String WSN::getChecksum()
+{
+	return checksum;
+}
+
+double WSN::get_xCoordVoltage()
+{
+	return xCoordVoltage;
+}
+
+double WSN::get_yCoordVoltage()
+{
+	return yCoordVoltage;
+}
+
+double WSN::get_zCoordVoltage()
+{
+	return zCoordVoltage;
+}
+
