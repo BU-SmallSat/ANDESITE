6bca758328edb46864d399d55b9a96288231e80a
Graham Voysey <gvoysey@bu.edu>
2017-01-24 17:54:02 -0500

    tuning setup.py

--- BBB_files/setup.py
+++ BBB_files/setup.py
@@ -4,14 +4,13 @@ from setuptools import setup, find_packages, Extension
 exec(open('MULE/_version.py').read())
 # http://python-packaging.readthedocs.org/en/latest/command-line-scripts.html
 
-
 setup(name='MULE',
       version=__version__,
       packages=find_packages(),
-      package_data={
-          'core': ['resources/*',
-                   'resources/tone_in_noise/*']
-      },
+      #package_data={
+      #    'MULE': ['resources/*',
+      #             'resources/tone_in_noise/*']
+      #},
       scripts=['scripts/mule'],
       #url='',
       #license='',
@@ -31,4 +30,5 @@ setup(name='MULE',
           'pytest',
           'hypothesis'
       ],
-      test_suite="py.test")
+      test_suite="py.test"
+      )
