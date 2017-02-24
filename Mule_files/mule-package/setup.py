f76de3474e542492ae54f8eaec1c20d3b9c00490
Graham Voysey <gvoysey@bu.edu>
2017-02-24 16:43:36 -0500

    futzing with layout

---
+++ Mule_files/mule-package/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages, Extension
+
+# makes __version__ a local variable
+exec(open('MULE/_version.py').read())
+# http://python-packaging.readthedocs.org/en/latest/command-line-scripts.html
+
+setup(name='MULE',
+      version=__version__,
+      packages=find_packages(),
+      #package_data={
+      #    'MULE': ['resources/*',
+      #             'resources/tone_in_noise/*']
+      #},
+      scripts=['scripts/mule'],
+      #url='',
+      #license='',
+      #ext_modules=cythonize(extensions),
+      #include_dirs=[np.get_include()],
+      #author='',
+      #author_email='',
+      #description=''
+      install_requires=[
+          'docopt >= 0.6',
+          'crc16',
+          'Adafruit_BBIO',
+          'pyserial',
+          'pexpect'
+      ],
+      tests_require=[
+          'pytest',
+          'hypothesis'
+      ],
+      test_suite="py.test"
+)
