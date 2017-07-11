e88c09ef5e03a4f4da0a67f1c09005c8c914e166
mlkromis <mlkromis@bu.edu>
2017-07-11 12:19:21 -0400

    Tidying up lose ends of git revamp

--- mule/tests/test_tobytes.py
+++
@@ -1,2 +0,0 @@
-LEN44 = (chr(0x00)+chr(0x00)+chr(0x00)+chr(0x2C)).encode("utf-8")
-print(LEN44[2:4])
--- mule/tests/testingInt.py
+++
@@ -1,2 +0,0 @@
-string = "1234".encode("utf8")
-print("CRC Integer: ", int(string.decode("utf8"), 10))
